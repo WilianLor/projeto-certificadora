197a1caea92a47ad26acda464a98d61ca6a0e8ff
Felipe Bras Coutinho <felipebrascoutinho@gmail.com>
2024-09-02 21:27:15 -0300

    Correção do ContextNewUser para que utilize a api ao cadastrar novos usuários.

--- apps/web/src/contexts/CreateNewUserContext.tsx
+++ apps/web/src/contexts/CreateNewUserContext.tsx
@@ -1,11 +1,11 @@
 import React, { createContext, ReactNode, useContext } from "react";
-import axios from "axios";
 import { toast } from "react-toastify";
-import { SessionContext } from "./SessionContext";
+import api, { ApiResponse } from "../services/api";  // Importe a instância configurada do axios
+import { TokensData } from "./types";
 
 // Definir o tipo do contexto
 interface CreateNewUserContextType {
-  createuser: (username: string, password: string) => void;
+  createuser: (username: string, password: string, isAdmin: number) => void; // Adicionei isAdmin
 }
 
 // Inicializar o contexto com um valor default
@@ -16,39 +16,29 @@ interface CreateNewUserProviderProps {
   children: ReactNode;
 }
 
-const url = process.env.REACT_APP_BACKEND_URL || "http://localhost:3030/";
-
 const CreateNewUserProvider: React.FC<CreateNewUserProviderProps> = ({ children }) => {
-  // Mover a chamada useContext para o corpo do componente
-  const {login} = useContext(SessionContext);
 
-  const createuser = (username: string, password: string) => {
-    const userData = { username, password };
+  const createuser = async (username: string, password: string, isAdmin: number) => {
+    const userData = { username, password, isAdmin }; // Inclua isAdmin no payload
 
-    axios.post(process.env.REACT_APP_BACKEND_URL || `${url}user/createuser`, userData)
-      .then(response => {
-        if (response.data.status !== "error") {
-          toast.success("Usuário criado com sucesso...");
+    try {
+      const response = await api.post<ApiResponse<TokensData>>("user/", userData);
 
-          setTimeout(() => {
-           
-              login(userData.username, userData.password);
-            
-          }, 3000);
+      if (response.data.status !== "error") {
+        toast.success("Usuário criado com sucesso...");
 
-        } else {
-          toast.warning(response.data.message);
-        }
-      })
-      .catch(error => {
-        if (error.response) {
-          console.error('Erro na resposta:', error.response.status, error.response.data);
-        } else if (error.request) {
-          console.error('Nenhuma resposta do servidor:', error.request);
-        } else {
-          console.error('Erro na requisição:', error.message);
-        }
-      });
+      } else {
+        toast.warning(response.data.error || "Erro ao criar usuário");
+      };
+    } catch (error: any) {
+      if (error.response) {
+        console.error('Erro na resposta:', error.response.status, error.response.data);
+      } else if (error.request) {
+        console.error('Nenhuma resposta do servidor:', error.request);
+      } else {
+        console.error('Erro na requisição:', error.message);
+      }
+    }
   };
 
   return (
